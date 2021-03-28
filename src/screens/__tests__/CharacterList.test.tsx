07598f92daa13a4826753a420ee2b90de3cfc92c
Douglas Delabrida <dougdelabrida@gmail.com>
2021-03-28 20:41:47 +0100

    test: test rendering of characters

---
+++ src/screens/__tests__/CharacterList.test.tsx
@@ -0,0 +1,28 @@
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { MockedProvider } from "@apollo/client/testing";
+import { CharacterList } from "../CharacterList";
+import { charactersMock } from "../../mocks";
+
+describe("<CharacterList />", () => {
+  test("should render a list of names", async () => {
+    render(
+      <MockedProvider mocks={charactersMock} addTypename={false}>
+        <CharacterList />
+      </MockedProvider>
+    );
+
+    await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
+
+    const rick = await screen.findByText("Rick Sanchez");
+    const morty = await screen.findByText("Morty Smith");
+    const summer = await screen.findByText("Summer Smith");
+
+    expect(rick).toBeInTheDocument();
+    expect(morty).toBeInTheDocument();
+    expect(summer).toBeInTheDocument();
+  });
+});
