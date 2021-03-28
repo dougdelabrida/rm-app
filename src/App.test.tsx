e7b431ab5495cb11dcd9415799831dfe6c5849c9
Douglas Delabrida <dougdelabrida@gmail.com>
2021-03-28 20:43:43 +0100

    test: check title of page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders the app", () => {
   render(<App />);
-  const linkElement = screen.getByText(/just a test/i);
+  const linkElement = screen.getByText(/Get Schwifty/i);
   expect(linkElement).toBeInTheDocument();
 });
