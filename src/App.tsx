2955130850d63854b9ab85899fcb9117df6f8e3b
Douglas Delabrida <dougdelabrida@gmail.com>
2021-03-28 20:43:11 +0100

    feature: render characters list on main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ApolloProvider } from "@apollo/client";
 import client from "./api/graphql";
+import { CharacterList } from "./screens/CharacterList";
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div>just a test</div>;
+      <h1>Get Schwifty</h1>
+      <CharacterList />
     </ApolloProvider>
   );
 }
