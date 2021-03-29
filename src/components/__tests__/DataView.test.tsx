fd7d93c7f8c5324ad3020668dc3a0756b57e6ab9
Douglas Delabrida <dougdelabrida@gmail.com>
2021-03-29 09:22:41 +0100

    test: ensure DataView is rendering properly

--- src/components/__tests__/DataView.test.tsx
+++ src/components/__tests__/DataView.test.tsx
@@ -1,17 +1,46 @@
 import { render } from "@testing-library/react";
 import { DataView, ItemView } from "../DataView";
 
+const data: ({ name: string } & ItemView)[] = [
+  { id: "test", name: "justa test" },
+  { id: "test1", name: "justa test1" },
+  { id: "test2", name: "justa test2" },
+  { id: "test3", name: "justa test3" },
+  { id: "test4", name: "justa test4" },
+  { id: "test5", name: "justa test5" },
+];
+
 describe("<DataView />", () => {
   test("should render without errors", () => {
     const data: ({ name: string } & ItemView)[] = [
       { id: "test", name: "justa test" },
     ];
     const { getByText } = render(
-      <DataView data={data} render={(value) => <div>{value.name}</div>} />
+      <DataView
+        data={data}
+        limit={1}
+        total={1}
+        onLoadMore={() => null}
+        render={(value) => <div>{value.name}</div>}
+      />
     );
 
-    const t = getByText(/justa test/i);
+    const element = getByText(/justa test/i);
 
-    expect(t).toBeInTheDocument();
+    expect(element).toBeInTheDocument();
+  });
+  test("should have a load more at bottom when total is higher than the displayed data", () => {
+    const { getByText } = render(
+      <DataView
+        data={data}
+        limit={1}
+        total={data.length}
+        onLoadMore={() => null}
+        render={(value) => <div>{value.name}</div>}
+      />
+    );
+
+    const loadMoreEl = getByText(/load more/i);
+    expect(loadMoreEl).toBeInTheDocument();
   });
 });
