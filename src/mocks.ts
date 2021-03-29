2625f1510b0aac9fa3ecc82c4e76c9987a295051
Douglas Delabrida <dougdelabrida@gmail.com>
2021-03-29 19:46:34 +0100

    test: update mocks

--- src/mocks.ts
+++ src/mocks.ts
@@ -5,16 +5,38 @@ export const charactersMock = [
     request: {
       query: GET_CHARACTERS,
       variables: {
-        page: 0,
+        page: 1,
       },
     },
     result: {
       data: {
         characters: {
+          info: {
+            __typename: "Info",
+            count: 671,
+          },
           results: [
-            { id: "1", name: "Rick Sanchez", __typename: "Character" },
-            { id: "2", name: "Morty Smith", __typename: "Character" },
-            { id: "3", name: "Summer Smith", __typename: "Character" },
+            {
+              id: "1",
+              name: "Rick Sanchez",
+              image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+              __typename: "Character",
+              status: "Alive",
+            },
+            {
+              id: "2",
+              name: "Morty Smith",
+              image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+              __typename: "Character",
+              status: "Alive",
+            },
+            {
+              id: "3",
+              name: "Summer Smith",
+              image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+              __typename: "Character",
+              status: "Alive",
+            },
           ],
           __typename: "Characters",
         },
